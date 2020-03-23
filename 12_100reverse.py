8a40ecea2ee4336df8cb2f4d8fa63b13720c0bad
ZiQ9 <62322258+ZiQ9@users.noreply.github.com>
2020-03-23 18:31:12 +0900

    Update 12_100reverse.py

--- 12_100reverse.py
+++ 12_100reverse.py
@@ -2,7 +2,6 @@ f = open('output.txt')
 l = f.readlines()
 f.close()
 
-data = []
 for i in reversed(range(0, 101)):
-    data.append(int(l[i]))
-    print(int(l[i]))
+    a = int(l[i]) * 2
+    print(a) 
